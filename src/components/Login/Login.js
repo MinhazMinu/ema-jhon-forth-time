1403fb918d9e6f4d64fbaaaf9cc926f7b372d7b8
minhaz <minhaz.taher@gmail.com>
2020-03-28 04:17:52 +0600

    call auth fucntion in login and display signIn pop up

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Login.css";
+import Auth from "./useAuth";
 
 const Login = () => {
-  return <div>login</div>;
+  const auth = Auth();
+  return (
+    <div>
+      <button
+        className="btn btn-sm btn-success"
+        onClick={auth.singInWithGoogle}
+      >
+        SignIn with Google
+      </button>
+    </div>
+  );
 };
 
 export default Login;
