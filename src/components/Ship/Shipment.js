e4c999f3ada7e4c287251b59d7050507f326be88
minhaz <minhaz.taher@gmail.com>
2020-03-28 16:02:49 +0600

    Adding User form on during check out

--- src/components/Ship/Shipment.js
+++ src/components/Ship/Shipment.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Shipment = () => {
-  return <div>ship</div>;
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+  };
+
+  console.log(watch("example")); // watch input value by passing the name of it
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input name="example" defaultValue="test" ref={register} />
+
+      <input name="exampleRequired" ref={register({ required: true })} />
+
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
+  );
 };
 
 export default Shipment;
