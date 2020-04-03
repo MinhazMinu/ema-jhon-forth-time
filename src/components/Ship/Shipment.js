a9c26a9f9db29bd8b4dc9cd7dee2d89357f265f5
minhaz <minhaz.taher@gmail.com>
2020-04-03 08:03:09 +0600

    stripe install and make sevtion in place order page

--- src/components/Ship/Shipment.js
+++ src/components/Ship/Shipment.js
@@ -9,7 +9,11 @@ const Shipment = () => {
   const onSubmit = data => {
     //TODO : Moved this after payment
     const savedCart = getDatabaseCart();
-    const orderDetails = { email: auth.user.email, cart: savedCart };
+    const orderDetails = {
+      email: auth.user.email,
+      cart: savedCart,
+      shipment: data
+    };
 
     fetch("http://localhost:4200/placeOrder", {
       method: "POST",
@@ -19,86 +23,97 @@ const Shipment = () => {
       body: JSON.stringify(orderDetails)
     })
       .then(res => res.json())
-      .then(data => {
-        alert("Successfully Placed your order with order id " + data._id);
+      .then(order => {
+        alert("Successfully Placed your order with order id " + order._id);
         processOrder();
       });
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="d-flex flex-column justify-content-center align-items-center  mt-5"
-    >
-      {" "}
-      <div className="w-2">
-        <input
-          className="form-control pt-2 m-2"
-          name="name"
-          placeholder="Name"
-          defaultValue={auth.user.name}
-          ref={register({ required: true })}
-        />
-        {errors.name && <span className="text-danger">Name is required</span>}
+    <div className="container">
+      <div className="row m-5">
+        <div className="col-md-6 border-right ">
+          <h4 className="text-center ">Shipping Information</h4>{" "}
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="d-flex flex-column justify-content-center align-items-center  mt-5"
+          >
+            <div className="w-2">
+              <input
+                className="form-control pt-2 m-2"
+                name="name"
+                placeholder="Name"
+                defaultValue={auth.user.name}
+                ref={register({ required: true })}
+              />
+              {errors.name && (
+                <span className="text-danger">Name is required</span>
+              )}
 
-        <input
-          className="form-control pt-2 m-2"
-          name="address"
-          placeholder="address"
-          ref={register({ required: true })}
-        />
-        {errors.address && (
-          <span className="text-danger">Address is required</span>
-        )}
+              <input
+                className="form-control pt-2 m-2"
+                name="address"
+                placeholder="address"
+                ref={register({ required: true })}
+              />
+              {errors.address && (
+                <span className="text-danger">Address is required</span>
+              )}
 
-        <input
-          className="form-control pt-2 m-2"
-          name="email"
-          placeholder="email"
-          defaultValue={auth.user.email}
-          ref={register({ required: true })}
-        />
+              <input
+                className="form-control pt-2 m-2"
+                name="email"
+                placeholder="email"
+                defaultValue={auth.user.email}
+                ref={register({ required: true })}
+              />
 
-        {errors.email && (
-          <span className="text-danger">Email field is required</span>
-        )}
-        <input
-          className="form-control pt-2 m-2"
-          name="city"
-          placeholder="city"
-          ref={register({ required: true })}
-        />
+              {errors.email && (
+                <span className="text-danger">Email field is required</span>
+              )}
+              <input
+                className="form-control pt-2 m-2"
+                name="city"
+                placeholder="city"
+                ref={register({ required: true })}
+              />
 
-        {errors.city && (
-          <span className="text-danger">City field is required</span>
-        )}
-        <input
-          className="form-control pt-2 m-2"
-          name="country"
-          placeholder="country"
-          ref={register({ required: true })}
-        />
+              {errors.city && (
+                <span className="text-danger">City field is required</span>
+              )}
+              <input
+                className="form-control pt-2 m-2"
+                name="country"
+                placeholder="country"
+                ref={register({ required: true })}
+              />
 
-        {errors.country && (
-          <span className="text-danger">Country field is required</span>
-        )}
-        <input
-          className="form-control pt-2 m-2"
-          name="zip"
-          placeholder="zipCode"
-          ref={register({ required: true })}
-        />
+              {errors.country && (
+                <span className="text-danger">Country field is required</span>
+              )}
+              <input
+                className="form-control pt-2 m-2"
+                name="zip"
+                placeholder="zipCode"
+                ref={register({ required: true })}
+              />
 
-        {errors.zip && (
-          <span className="text-danger">ZipCode field is required</span>
-        )}
+              {errors.zip && (
+                <span className="text-danger">ZipCode field is required</span>
+              )}
 
-        <input
-          className="form-control pt-2 m-2 btn btn-xs btn-warning rounded "
-          type="submit"
-        />
+              <input
+                className="form-control pt-2 m-2 btn btn-xs btn-warning rounded "
+                type="submit"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="col-md-6 d-flex justify-content-center ">
+          <h4>Payment Information</h4>
+        </div>
       </div>
-    </form>
+    </div>
   );
 };
 
